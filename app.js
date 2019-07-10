9e6cf7ce64301be6991133f1e9ac9e667b8072ab
lawgotcode <lawgotcode1990@gmail.com>
2019-07-09 23:24:23 -0500

    functions

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const game = ()=> {
   let pScore = 0;
   let cScore = 0;
-
+ // start the game
   const startGame = () =>{
   	const playBtn = document.querySelector(".intro button");
   	const introScreen = document.querySelector(".intro");
@@ -13,12 +13,31 @@ const game = ()=> {
   	});
   };
 
+  // play match
+  const playMatch = () => {
+  	const options = document.querySelectorAll(".options button");
+  	const playerHand = document.querySelector(".player-hand");
+  	const computerHand = document.querySelector(".computer-hand");
+  	// computer options
+  	const computerOptions = ['rock', 'papers', 'scissors'];
 
+  	const computerNumber = Math.floor(Math.random() * 3);
+
+  	options.forEach(option=>{
+       option.addEventListener("click", function() {
+        // computer choice
+       	const computerNumber = Math.floor(Math.random() * 3);
+       	const computerCjoice = computerOptions[computerNumber];
+       	console.log(computerChoise);
+       });
+  	});
+  };
 
 
 
  //is call the inner function
  startGame();
+ playMatch()
 };
 
 // here starts the game
